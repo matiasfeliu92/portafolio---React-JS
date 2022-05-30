615f27050749fae5882e544b005bcd610a46f2f8
matiasfeliu92 <matumazparrote@gmail.com>
2022-05-30 13:52:38 -0300

    agrego nuevo trabajo en componente trabajos realizados

--- src/componentes/Trabajos.js
+++ src/componentes/Trabajos.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import webArq from "../assets/videos/20220504_123335.mp4"
-import appCanciones from "../assets/images/appDeCanciones.PNG"
+import appCanciones from "../assets/videos/20220506_123752.mp4"
 import webMates from "../assets/videos/20220504_123203.mp4"
 import webLibreria from "../assets/videos/20220504_123452.mp4"
 import webForm from "../assets/videos/20220504_123002.mp4"
 import webPort from "../assets/videos/20220504_122700.mp4"
+import appDisney from "../assets/videos/20220530_104823.mp4"
 // import workProgressGif from "../assets/images/Work-In-Progress.png"
 import './trabajosRealiz.css';
 
 const Trabajos = () => {
 
     const trabajos = [
-        {titulo: "App de disney", descripcion1: "Consiste en una app realizada para Alkemy, donde se pueden guardar y editar los personajes de Disney", descripcion2: "Tecnologias usadas: Node Js, Express y MySql", link: "", rutaVideo: "", linkGit: "https://github.com/matiasfeliu92/Challenge-Backend-Alkemy--Node-Js"},
+        {titulo: "App de Disney", descripcion1: "Consiste en una lista de personajes de Disney, donde se pueden crear nuevos personajes, mostrar, actualizar y eliminar", descripcion2: "Tecnologias usadas: Node Js, Express, MySql con Sequelize y Mocha para los test de los endpoints", link: "", rutaVideo: appDisney, linkGit: "https://github.com/matiasfeliu92/Challenge-Backend-Alkemy--Node-Js"},
         {titulo: "App de canciones", descripcion1: "Consiste en una lista de musica, donde se pueden guardar y editar las canciones favoritas.", descripcion2: "Tecnologias usadas: Node Js, Express y MySql", link: "", rutaVideo: appCanciones, linkGit: "https://github.com/matiasfeliu92/Proyecto-1-bootcamp-Node-Js-Fundamentals"},
         {titulo: "Web de libreria tecnica", descripcion1: "Pagina web e-commerce de una libreria, realizada como trabajo final para el curso de JavaScript", descripcion2: "Tecnologias usadas: Html 5, Css 3 y JavaScript con libreria JQuery", link: "https://libreria-tecnica.netlify.app/", rutaVideo: webLibreria, linkGit: "https://github.com/matiasfeliu92/WEB-LIBRERIA-TECNICA-VERSION-FINAL"},
         {titulo: "Portafolio de arquitectura", descripcion1: "Pagina web de portafolio para arquitectos, realizada como trabajo final para el curso de Desarrollo Web", descripcion2: "Tecnologias usadas: Html 5, Css 3 con Bootstrap 5, Sass y JavaScript con libreria JQuery", link: "https://arq-matias-mazparrote-feliu.netlify.app/", rutaVideo: webArq, linkGit: "https://github.com/matiasfeliu92/Repositorio-Avances-Proyecto-Curso-HTML-y-CSS"},
